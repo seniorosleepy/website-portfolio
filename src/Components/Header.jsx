b4bf322e4685a0740390a74d8c85324cc6d91796
senior <isleepy72@gmail.com>
2025-12-15 17:21:49 +0400

    add language switcher

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [lang, setLang] = useState("en");
+
+function changeLanguage() {
+  setLang(prev => (prev === "en" ? "ru" : "en"));
+}
+
+
   return (
     <div className="header-section">
-      <a href="#" className="header-links">About me</a>
-      <a href="#" className="header-links">Portfolio</a>
-      <a href="#" className="header-links">Skills</a>
-      <a href="#" className="header-links">Contact</a>
+      <button className="lang-button" onClick={changeLanguage}>
+        lang
+      </button>
+      <div className="header-links">
+        <a href="#" className="header-link">
+          {lang === "en" ? "About me" : "Обо мне"}
+        </a>
+        <a href="#" className="header-link">
+          {lang === "en" ? "Portfolio" : "Портфолио"}
+        </a>
+        <a href="#" className="header-link">
+          Skills
+        </a>
+        <a href="#" className="header-link">
+          Contact
+        </a>
+      </div>
     </div>
   );
 }
